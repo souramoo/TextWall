a3456304acfeca965e04d4265923f8f0fcd3735c
Souradip Mookerjee <souramoo@gmx.com>
2018-04-15 16:57:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const bodyParser = require('body-parser')
 
+function handleInboundSms(request, response) {
+  const params = Object.assign(request.query, request.body)
+  console.log(params)
+  io.emit('chat message', '<b>+' + params.msisdn + "</b>: " + params.text);
+  response.status(204).send()
+}
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -11,17 +18,12 @@ app.route('/webhooks/inbound-sms')
    .get(handleInboundSms)
    .post(handleInboundSms)
 
-function handleInboundSms(request, response) {
-  const params = Object.assign(request.query, request.body)
-  console.log(params)
-  io.emit('chat message', '<b>+' + params.msisdn + "</b>: " + params.text);
-  response.status(204).send()
-}
-
+// ui
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// go go go
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
