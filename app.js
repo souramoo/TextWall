46f2ff96369173f479cc8824fdce46289cbdea56
Souradip Mookerjee <souramoo@gmx.com>
2018-04-15 16:47:45 +0100

    Added code

---
+++ app.js
@@ -0,0 +1,33 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// nexmo inbound sms webhook
+app
+  .route('/webhooks/inbound-sms')
+  .get(handleInboundSms)
+  .post(handleInboundSms)
+
+function handleInboundSms(request, response) {
+  const params = Object.assign(request.query, request.body)
+  console.log(params)
+  io.emit('chat message', '<b>+' + params.msisdn + "</b>: " + params.text);
+  response.status(204).send()
+}
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
